1f4cfcf9aba994737eda4fa0f0312814d7725224
nikitamekh <nikita.mekh@gmail.com>
2018-06-26 12:52:38 +0300

    1. Исправлена ошибка с автоматическим обновлением статуса заказа

--- esasby.hutkigrosh/install/php_interface/include/sale_payment/hutkigrosh/result_rec.php
+++ esasby.hutkigrosh/install/php_interface/include/sale_payment/hutkigrosh/result_rec.php
@@ -27,7 +27,11 @@ if (empty($hgBillInfo)) {
     throw new Exception($error);
 } else {
     $hg->apiLogOut();
-    $localOrderInfo = CSaleOrder::GetByID($hgBillInfo['invId']);
+    // первым делом пытаемся получить заказ по account_number (случай, когда в магазине включен шаблон генерации номер счета)
+    $orderByAccount = \Bitrix\Sale\Order::loadByAccountNumber($hgBillInfo['invId']);
+    // т.к. нам нужен массив, а не объект \Bitrix\Sale\Order загружаем массив по id.
+    // если по account_number заказ не был найден, то пытаемся закгрузить по invId
+    $localOrderInfo = CSaleOrder::GetByID(!empty($orderByAccount) ? $orderByAccount->getId() : $hgBillInfo['invId']);
     if ($localOrderInfo['USER_NAME'] . ' ' . $localOrderInfo['USER_LAST_NAME'] != $hgBillInfo['fullName']
         && $localOrderInfo['PRICE'] != $hgBillInfo['amt']) {
         throw new Exception("Unmapped purchaseid");
